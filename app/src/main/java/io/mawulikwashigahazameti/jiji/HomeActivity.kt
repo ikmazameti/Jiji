a257d05be2a6a925c8181b0520b0828acb11d5de
isaac Azameti <60759789+ikmazameti@users.noreply.github.com>
2022-08-16 15:53:47 +0000

    added product item

--- app/src/main/java/io/mawulikwashigahazameti/jiji/HomeActivity.kt
+++ app/src/main/java/io/mawulikwashigahazameti/jiji/HomeActivity.kt
@@ -1,8 +1,13 @@
 package io.mawulikwashigahazameti.jiji
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.mawulikwashigahazameti.jiji.databinding.ActivityHomeBinding
+
 
 class HomeActivity : AppCompatActivity() {
 
@@ -16,6 +21,29 @@ class HomeActivity : AppCompatActivity() {
 
         setSupportActionBar(findViewById(R.id.toolbar))
         binding.toolbarLayout.title = title
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
+
+
+        bottomNav.setOnItemReselectedListener {
+            when (it.itemId) {
+                R.id.action_home -> {
+                    Toast.makeText(this, "home", Toast.LENGTH_SHORT).show()
+                }
+                R.id.action_messages -> {
+                    Toast.makeText(this, "message", Toast.LENGTH_SHORT).show()
+                }
+                R.id.action_sell -> {
+                    Toast.makeText(this, "sell", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
 
     }
 }
---
+++ app/src/main/java/io/mawulikwashigahazameti/jiji/SellFragment.kt
@@ -0,0 +1,32 @@
+package io.mawulikwashigahazameti.jiji
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SellFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SellFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sell, container, false)
+    }
+
+
+}
